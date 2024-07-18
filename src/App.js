6453bd81469dc5758e45a65e06320780863a20a7
MaskeVinod <vinodmaske1012@gmail.com>
2024-07-18 11:04:17 +0530

    Higher Order Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HocRed comp={Counter} />
+      <HocGreen comp={Counter} />
+      <HocBlue comp={Counter} />
+    </>
+  );
+}
+
+function HocRed(props) {
+  return (
+    <h2
+      style={{
+        width: 100,
+        height: 150,
+        backgroundColor: "red",
+        margin: 10,
+        textAlign: "center",
+      }}
+    >
+      {<props.comp />}
+    </h2>
+  );
+}
+function HocGreen(props) {
+  return (
+    <h2
+      style={{
+        width: 100,
+        height: 150,
+        backgroundColor: "green",
+        margin: 10,
+        textAlign: "center",
+      }}
+    >
+      {<props.comp />}
+    </h2>
+  );
+}
+function HocBlue(props) {
+  return (
+    <h2
+      style={{
+        width: 100,
+        height: 150,
+        backgroundColor: "blue",
+        margin: 10,
+        textAlign: "center",
+      }}
+    >
+      {<props.comp />}
+    </h2>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Update</button>
+    </>
   );
 }
 
